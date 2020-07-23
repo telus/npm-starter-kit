4090c10e8589fc4827e6a44bbb790cf6cf511edf
Matthew Cantelon <mattcan@users.noreply.github.com>
2020-07-23 08:55:46 -0700

    feat: remove forced table formatting

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ module.exports.plugins = [
   ['lint-list-item-spacing', false],
   ['lint-maximum-line-length', false],
   ['lint-emphasis-marker', '_'],
-  ['lint-ordered-list-marker-value', 'ordered']
+  ['lint-ordered-list-marker-value', 'ordered'],
+  ["remark-lint-table-pipe-alignment", false],
+  ["remark-lint-table-cell-padding", false]
 ]
